0fb1ae47a8de6bb8bd0d179f4e65878d707fae71
Deonn Francis Almirol <77103235+sirloinsteak11@users.noreply.github.com>
2023-06-01 22:37:35 +0000

    aerobesoi;hbaettboih

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log('test')
+/* for (let i = 0; i < 7; i++) {
+    let featItemList = document.getElementsByClassName('featured-items-cells');
+    let textBox = document.createElement('div');
+    textBox.setAttribute('id',`featured-item-cell${i}`);
+    textBox.setAttribute('style','width: 100px; height: 100px color: pink; border: 1px solid black');
+    console.log(textBox.id);
+    featItemList.appendChild(textBox);
+} */
+
+document.getElementById('title').innerHTML = "HEEELLLPP FUKFUCKFUF";
